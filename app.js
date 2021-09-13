09c590614d8c3a27b2869b66425ecc9d0046da4d
Cai <trieungochai.dev@gmail.com>
2021-09-13 15:26:09 +0700

    Adding Success and Failure Pages

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.post("/", function (req, res) {
   };
 
   const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
@@ -45,6 +51,10 @@ app.post("/", function (req, res) {
 
   request.write(jsonData);
   request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
